ea14a0a2d7b1396824d272b3e3bb7160c2efb3e9
HakimP <pedrohakim0@gmail.com>
2016-07-07 21:08:51 -0500

    ...

--- js/script.js
+++ js/script.js
@@ -11,17 +11,25 @@
 // 		iteradorMeses();
 // 	}
 // }
+//DB SCHEMA
+var arraySentimientos = [["contento",["contento", "contento2", "contento3", "contento4", "contento5"]], 
+						 ["triste",["triste", "triste2", "triste3", "triste4", "triste5"]], 
+						   ["enamorado",["enamorado", "enamorado2", "enamorado3", "enamorado4", "enamorado5"]], 
+						   ["odio",["odio", "odio2", "odio3", "odio4", "odio5"]], 
+						   ["intenso",["intenso", "intenso2", "intenso3", "intenso4", "intenso5"]], 
+						   ["soledad",["soledad", "soledad2", "soledad3", "soledad4", "soledad5"]], 
+						   ["angustia",["angustia", "angustia2", "angustia3", "angustia4", "angustia5"]],
+						   ["",["c","c","c","c","c"]]
+						];
 
-var arraySentimientos = ["contento", "triste", "enamorado", "odio", "intenso", "soledad", "angustia"];
 var arraySaludos = ["hola", "hey"];
-
 var contandor = 0;
 
 var banderaHola = false;
 
 //5 para res sent y 7 para sent
 function valorRandom(numeroRandom) {
-    var x = Math.floor((Math.random() * numeroRandom) + 1);
+    var x = Math.floor((Math.random() * numeroRandom));
     return x;
 }
 
@@ -32,75 +40,58 @@ function buscarContenido (palabra, cadenaCarac) {
 
 function respondePregunta() {
 	var numero = prompt("Pregunta Algo", "");
-	
+
+
 	for(var i = 0 ; i <= arraySaludos.length-1; i++) {
 		if(buscarContenido(arraySaludos[i], numero.toLowerCase() ) != -1) {
 			console.log("Hola!!!");
 			banderaHola = true;
 		}
 	}
-	if(banderaHola) {
+
+	// if(banderaHola) {
 		for(var i = 0 ; i <= arraySentimientos.length-1; i++) {
-			if(buscarContenido(arraySentimientos[i], numero.toLowerCase() ) != -1) {
+			if(buscarContenido(arraySentimientos[i][0], numero.toLowerCase()) != -1 && banderaHola) {
 				// console.log("si existe el Sent");
-				switch(arraySentimientos[i]) {
+				switch(arraySentimientos[i][0]) {
 				    case "contento":
-				        console.log(respuestasContento());
+				        console.log(respuestasSentimientos(i));
 				        break;
 				    case "triste":
-				        console.log("Pregunta mas triste");
+				        console.log(respuestasSentimientos(i));
+				        break;
+				    case "enamorado":
+				        console.log(respuestasSentimientos(i));
+				        break;
+				    case "odio":
+				        console.log(respuestasSentimientos(i));
+				        break;
+				    case "intenso":
+				        console.log(respuestasSentimientos(i));
+				        break;
+				    case "soledad":
+				        console.log(respuestasSentimientos(i));
+				        break;
+				    case "angustia":
+				        console.log(respuestasSentimientos(i));
 				        break;
 				    default:
-				        console.log("Pregunta CON SENTIMIENTO");
+				        console.log("Pregunta random");
 				        break; 
 				}
-			} else{
+			} else if (!banderaHola){
+				 console.log("Pregunta otra vez por que no existes");
 			}
 		}
-	}
+	// }
 	respondePregunta();
 }
 
-function respuestasContento() {
-
-	switch(valorRandom(5)) {
-	    case 1:
-	        console.log("Pregunta mas Contento 1");
-	        break;
-	    case 2:
-	        console.log("Pregunta mas Contento 2");
-	        break;
-	    case 3:
-	        console.log("Pregunta mas Contento 3");
-	        break;
-        case 4:
-	        console.log("Pregunta mas Contento 4");
-        	break;
-        case 5:
-	        console.log("Pregunta mas Contento 5");
-	        break;
-
-	}
+function respuestasSentimientos(sentimi) {
+	var randomActual = valorRandom(5);
+	console.log(arraySentimientos[sentimi][1][randomActual]);
 }
 
-function respuestasTRiste() {
-
-	switch(valorRandom(5)) {
-	    case 1:
-	        console.log("Pregunta mas Triste 1");
-	        break;
-	    case 2:
-	        console.log("Pregunta mas Triste 2");
-	        break;
-	    case 3:
-	        console.log("Pregunta mas Triste 3");
-	        break;
-        case 4:
-	        console.log("Pregunta mas Triste 4");
-        	break;
-        case 5:
-	        console.log("Pregunta mas Triste 5");
-	        break;
-
-	}
+function alerta() {
+	alert(arraySentimientos[0][0]  + " espacio " + arraySentimientosV2[0][1][1]);
 }
